fb6317a603de0278487adbd9a502cca08bcf556e
SMinhChau <minhchaunguyenmtp@gmail.com>
2022-10-24 09:32:41 +0700

    create project

---
+++ src/main/java/com/example/gatewaycloud/GatewaycloudApplication.java
@@ -0,0 +1,13 @@
+package com.example.gatewaycloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewaycloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewaycloudApplication.class, args);
+	}
+
+}
